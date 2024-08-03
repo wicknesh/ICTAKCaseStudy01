9f4f5e39f967ccf9080442f3e902db3292bf2b73
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-08-03 09:18:40 +0530

    Program to iterate from 0 to 15 and check for odds and evens

---
+++ OddEvenIterate.js
@@ -0,0 +1,3 @@
+for(let i = 0; i <=15 ; i++){ //For-loop that iterates from 0 to 15
+    i%2==0?console.log(`${i} is even.`):console.log(`${i} is odd.`); //Ternary operation to check and print odd and even numbers
+}
