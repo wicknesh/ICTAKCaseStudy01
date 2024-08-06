0cf9b202fa93a4f3892a9f3938e3ec8c80db214e
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-08-06 08:18:13 +0530

    Updated code to check for multiple most frequencies Declared check variable outside of for-of loop

--- freqARR.js
+++ freqARR.js
@@ -1,13 +1,14 @@
-let arr =  [1,1,2,2,2,2,3,3,4,5,5,5,5,5]; //Array
+let arr =  [1,1,2,2,2,2,3,3,3,3,4,5,5,5,5]; //Array
 Mfreq(arr); //Function call
 
 function Mfreq(arr){ //Function
     let mCount = 0; //Variable to store the most frequency encountered
     let freq = {}; //Object to store item : frequency pairs
-    let freqItem = 0; //Variable to store the most frequent item
+    let freqItem = []; //Array to store the most frequent item
+    let check; //Variable to store items from array
 
     for (let x of arr){
-        let check = x;
+        check = x;
         if(freq[check]){
             freq[check] += 1;
         }
@@ -16,8 +17,13 @@ function Mfreq(arr){ //Function
         }
         if(freq[check] > mCount){ //if-condition to check for the most frequency and store the most frequent item
             mCount = freq[check];
-            freqItem = check;
+            freqItem = []; //Empty array
+            freqItem.push(check);
+            //freqItem = check;
+        }
+        else if(freq[check] == mCount) { //else-if condition to check for same frequency
+            freqItem.push(check);
         }
     }
-    console.log(`${freqItem} is the most frequent item`); //Printing to console
+    console.log(`${freqItem} is/are the most frequent item/s.`); //Printing to console
 }
